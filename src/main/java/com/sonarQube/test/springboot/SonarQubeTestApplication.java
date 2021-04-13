d1fc7dc64593435a30240f7686eafc73bb3b56d6
Tom Apeltauer <t.apeltauer@seznam.cz>
2021-04-13 22:06:29 +0200

    ADD: project initialization
    * Eclipse 2021-03 + Maven + SpringBoot

---
+++ src/main/java/com/sonarQube/test/springboot/SonarQubeTestApplication.java
@@ -0,0 +1,13 @@
+package com.sonarQube.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQubeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarQubeTestApplication.class, args);
+	}
+
+}
